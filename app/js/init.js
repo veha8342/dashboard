adbb694c9fe9a31e4e5f6f94def7531b5a6e7ffa
veha8342 <veha1986@gmail.com>
2017-11-30 23:10:25 +0200

    modal start

--- app/js/init.js
+++ app/js/init.js
@@ -1,4 +1,6 @@
 $( function() {
+
+    //datepicker init
     var dateFormat = "mm/dd/yy",
         from = $( "#from" )
             .datepicker({
@@ -27,5 +29,45 @@ $( function() {
         }
 
         return date;
+
+    }
+
+    //chart init
+    // chart init
+    var ctx = document.getElementById("myChart");
+    if (ctx){
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["01", "02", '03', '04', '05', '06', '07', '08', '09', '10'],
+                datasets: [
+                    {
+                        label: '# of Votes',
+                        data: [100, 190, 120, 200, 210, 150, 175, 143, 110, 130],
+                        backgroundColor: [
+                            'rgba(128, 191, 202, 1)',
+                            'rgba(255, 154, 56, 1)',
+                            'rgba(128, 191, 202, 1)',
+                            'rgba(255, 154, 56, 1)',
+                            'rgba(128, 191, 202, 1)',
+                            'rgba(255, 154, 56, 1)',
+                            'rgba(128, 191, 202, 1)',
+                            'rgba(255, 154, 56, 1)',
+                            'rgba(128, 191, 202, 1)',
+                            'rgba(255, 154, 56, 1)',
+                        ]
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
     }
 } );
