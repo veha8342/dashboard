1e6d93a03c9274013c14213ba1603aa497a9342b
veha8342 <veha1986@gmail.com>
2018-10-05 16:25:05 +0300

    resize sidebar

--- src/js/plugin.js
+++ src/js/plugin.js
@@ -3,10 +3,30 @@
     var menuBtn = $(".menu-btn");
     var body = $("body");
     var mobileSidebarClass = '_mobileSidebar';
+    var mobileBreakPoint = 1280;
+    var openInMobileClass = '_openInMobile';
 
     menuBtn.on('click', function(e){
         // console.log('click');
+        var width = $(window).width();
         body.toggleClass(mobileSidebarClass);
+        if(width < mobileBreakPoint){
+            body.toggleClass(openInMobileClass);
+        }
+
+    });
+
+    $(window).on('resize', function (e) {
+        var width = $(this).width();
+        console.log(width);
+
+        if(width < mobileBreakPoint && !body.hasClass(openInMobileClass)){
+            body.addClass(mobileSidebarClass);
+
+        } else if(width > mobileBreakPoint ){
+            body.removeClass(mobileSidebarClass);
+            body.removeClass(openInMobileClass);
+        }
     });
 
 })();
